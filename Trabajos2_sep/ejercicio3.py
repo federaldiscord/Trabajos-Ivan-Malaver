d005e87b4fbbd817de8d383e7c871c1fb2f3ac4b
federaldiscord <90936645+federaldiscord@users.noreply.github.com>
2025-09-02 16:08:43 -0500

    Update ejercicio3.py

--- Trabajos2_sep/ejercicio3.py
+++ Trabajos2_sep/ejercicio3.py
@@ -1,9 +1,11 @@
 def verif_Edad(edad):
     if edad >= 18:
         return "es mayor de edad"
-    elif edad < 0:
+    elif edad < 18:
         return "es menor de edad"
 
 edad = int(input("Ingrese su edad: "))
+if edad != int(edad) or edad < 0:
+    print("Edad no valida")
 
 print(verif_Edad(edad))
